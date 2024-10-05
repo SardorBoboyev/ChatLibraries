a79474f9be99e13497b348f71f1c5233e80a543f
raykhonaNorboeva <raykhonanorboeva050@gmail.com>
2024-10-05 12:52:51 +0500

    create dtos

---
+++ src/main/java/uz/sb/domain/dto/response/ContactResponse.java
@@ -0,0 +1,10 @@
+package uz.sb.domain.dto.response;
+
+public class ContactResponse extends BaseResponse {
+
+    private Long userId; // adder
+
+    private Long contactUserId;  // person to join contact
+
+    private String contactName;
+}
